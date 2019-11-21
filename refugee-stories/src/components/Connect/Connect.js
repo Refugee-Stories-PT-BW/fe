0770fab904aa318b905c785bbcfff5c9dec90796
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-11-20 19:13:38 -0500

    changed the height of the div style in Connect

--- refugee-stories/src/components/Connect/Connect.js
+++ refugee-stories/src/components/Connect/Connect.js
@@ -47,7 +47,7 @@ const WrappedMap = withScriptjs(withGoogleMap(Map))
 
 export default function Connect() {
     return (
-        <div style={{ width:'100vw', height: '200vh'}} >
+        <div style={{ width:'100vw', height: '100vh'}} >
             <WrappedMap 
             googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
                 process.env.REACT_APP_GOOGLE_KEY
