856570e34e543940179683ef35d9b2853d6c4891
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-11-21 21:41:33 -0500

    Temporarily added the api_key

--- refugee-stories/src/components/Connect/Connect.js
+++ refugee-stories/src/components/Connect/Connect.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from 'react-google-maps'
 import * as nonprofitsData from '../../data/nonprofits.json'
 import mapStyle from './MapStyle'
+import { api_key } from '../../utils/data'
 
 function Map() {
     const [selectedOrg, setSelectedOrg] = useState(null);
@@ -49,13 +50,11 @@ export default function Connect() {
     return (
         <div style={{ width:'100vw', height: '100vh'}} >
             <WrappedMap 
-            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
-                process.env.REACT_APP_GOOGLE_KEY
-            }`}
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${api_key}`}
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `100%` }} />}
             mapElement={<div style={{ height: `100%` }} />}
              />
         </div>
     )
-}
+} 
